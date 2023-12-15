901b7c32788bbd5cacb25c2f8a2241403336b6f0
Eswar563 <eswar.p@access-designs.com>
2023-12-16 00:26:44 +0530

    added server

--- server/index.js
+++ server/index.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const app = express();
-const cors = require("cors")
-app.use(cors())
-const port = process.env.PORT||4000
+const cors = require("cors");
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 4000;
 const userRoutes = require('./routes/userRoutes.js');
 const mediaRoutes = require('./routes/mediaRoutes.js');
-app.use(express.json())
 app.use('/', userRoutes, mediaRoutes);
-const { request } = require("http");
-const { response } = require("express");
-const { title } = require("process");
-const console = require("console");
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/my-portfolio');
 
-
+const atlasConnectionUri = process.env.ATLAS_CONNECTION_STRING || 'mongodb://localhost:27017/my-portfolio';
+console.log(atlasConnectionUri)
 const initializeDBAndServer = async () => {
   try {
+    await mongoose.connect(atlasConnectionUri, {
+      useNewUrlParser: true, // no longer required
+      useUnifiedTopology: true, // no longer required
+    });
+
     app.listen(port, () => {
-      console.log("Server Running at http://localhost:4000/");
+      console.log(`Server Running at http://localhost:${port}/`);
+      console.log(`Connected to MongoDB at: ${atlasConnectionUri}`);
     });
   } catch (e) {
     console.log(`DB Error: ${e.message}`);
@@ -27,21 +32,3 @@ const initializeDBAndServer = async () => {
 };
 
 initializeDBAndServer();
-
-
-  
-
-
-
-
-
-
-
-
-
-
-      
-      
-      
-      
-      
